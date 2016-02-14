ca98e44777315f325201299cf138ec9df552cec3
mavame <mvanmeter1@gmail.com>
2016-02-14 15:21:33 -0500

    compresses css, removes unneeded stylesheet

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,9 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+// Change variable to 'expanded' in development.
+var outputStyle = 'compressed';
+
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -16,7 +19,7 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src("./sass/**/*.scss")
-        .pipe(sass())
+        .pipe(sass({outputStyle: outputStyle}))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
